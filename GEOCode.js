7e11a269cf92c36ecf9edb523958d9a46c8187ef
JamiePMortimer <16468666+JamiePMortimer@users.noreply.github.com>
2021-03-03 20:38:31 +0000

    Updated API output to capture lat lng

--- GEOCode.js
+++ GEOCode.js
@@ -2,12 +2,19 @@ const API2 = {
   key: '69adfaa55e574e9bb954810d342d6fe7',
   base: 'https://api.opencagedata.com/geocode/v1/json?q='
 };
+let lat;
+let lng;
 
 function geoResults(location) {
   fetch(`${API2.base}${location}&key=${API2.key}`).then((location) => {
-    console.log(location.json());
-  });
-  // }).then(displayResults);
+    return location.json();
+  }).then(displayResult);
 }
 
-geoResults('London');
+function displayResult (location){
+  lat = location.results[0].geometry.lat;
+  lng = location.results[0].geometry.lng;
+  console.log(lat, lng)
+}
+
+geoResults('London, UK');

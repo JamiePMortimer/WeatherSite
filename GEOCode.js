f300e2f4bb4f225367d04110d35db6d52d9fbb14
JamiePMortimer <16468666+JamiePMortimer@users.noreply.github.com>
2021-03-03 15:10:19 +0000

    Updated GEOCode API code

--- GEOCode.js
+++ GEOCode.js
@@ -1,15 +1,13 @@
 const API2 = {
-  key: "69adfaa55e574e9bb954810d342d6fe7",
-  base: "https://api.opencagedata.com/geocode/v1/json?q=",
-  test: "https://api.opencagedata.com/geocode/v1/json?q=Rua+Cafel%C3%A2ndia%2C+Carapicu%C3%ADba%2C+Brasil&key=69adfaa55e574e9bb954810d342d6fe7&pretty=1"
+  key: '69adfaa55e574e9bb954810d342d6fe7',
+  base: 'https://api.opencagedata.com/geocode/v1/json?q='
+};
+
+function geoResults(location) {
+  fetch(`${API2.base}${location}&key=${API2.key}`).then((location) => {
+    console.log(location.json());
+  });
+  // }).then(displayResults);
 }
 
-
-function geoResults(){
-  fetch(`${API2.test}`)
-  .then(location => {
-    console.log(location);})
-  // }).then(displayResults);
-  }
-
-geoResults();
+geoResults('London');

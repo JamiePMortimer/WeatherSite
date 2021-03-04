137f333a341c47fe8fb794660b026fcbe2cae4a4
JamiePMortimer <16468666+JamiePMortimer@users.noreply.github.com>
2021-03-04 20:01:59 +0000

    Added menu

--- app.js
+++ app.js
@@ -1,54 +1,77 @@
 const API = {
-  key: "3c6b6453b9930344c0199f22529f0a0e",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
-const city = document.querySelector('.output-location__city')
+  key: '3c6b6453b9930344c0199f22529f0a0e',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+const city = document.querySelector('.output-location__city');
 const searchInput = document.querySelector('.input__search-box');
-searchInput.addEventListener('keypress', weatherQuery)
+searchInput.addEventListener('keypress', weatherQuery);
 
-function weatherQuery (e) {
- if(e.keyCode == 13){
-   getResults(searchInput.value)
- }
+function weatherQuery(e) {
+  if (e.keyCode == 13) {
+    getResults(searchInput.value);
+  }
 }
 
-function getResults(query){
-fetch(`${API.base}weather?q=${query}&units=metric&appid=${API.key}`)
-.then(weather => {
-  return weather.json();
-}).then(displayResults);
+function getResults(query) {
+  fetch(`${API.base}weather?q=${query}&units=metric&appid=${API.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
 }
 
-function displayResults(weather){
-  document.querySelector('.output-location__city').textContent = `${weather.name}, ${weather.sys.country}`
-  const tempNow = Math.round(weather.main.temp)
-  const tempmaxNow = Math.round(+weather.main.temp_max)
-  const tempminNow = Math.round(+weather.main.temp_min)
-  document.querySelector('.temp').innerHTML = `${tempNow}<span>°C</span>`
-  document.querySelector('.hi-lo').textContent = `${tempminNow}°C / ${tempmaxNow}°C`
-  document.querySelector('.weather').textContent = `${weather.weather[0].main}`
+function displayResults(weather) {
+  document.querySelector(
+    '.output-location__city'
+  ).textContent = `${weather.name}, ${weather.sys.country}`;
+  const tempNow = Math.round(weather.main.temp);
+  const tempmaxNow = Math.round(+weather.main.temp_max);
+  const tempminNow = Math.round(+weather.main.temp_min);
+  document.querySelector('.temp').innerHTML = `${tempNow}<span>°C</span>`;
+  document.querySelector(
+    '.hi-lo'
+  ).textContent = `${tempminNow}°C / ${tempmaxNow}°C`;
+  document.querySelector('.weather').textContent = `${weather.weather[0].main}`;
 }
 
-let dateNow = function (){
+let dateNow = function () {
   let now = new Date();
-  let date = document.querySelector('.output-location__date')
+  let date = document.querySelector('.output-location__date');
   date.innerText = datebuilder(now);
-}
+};
 
-function datebuilder(d){
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-const days = ["Sun","Mon","Tues","Wed","thurs","Fri","Sat"];
-let day = days[d.getDay()];
-let date = d.getDate();
-let month = months[d.getMonth()];
-let year = d.getFullYear();
-return `${day} ${date} ${month}, ${year} `
+function datebuilder(d) {
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const days = ['Sun', 'Mon', 'Tues', 'Wed', 'thurs', 'Fri', 'Sat'];
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+  return `${day} ${date} ${month}, ${year} `;
 }
 
 dateNow();
 
-
 const burger = document.querySelector('.navigation-burger');
-burger.addEventListener('click', () =>{
-burger.classList.toggle('toggle')
-})
+const menu = document.querySelector('menu');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('toggle');
+  if (menu.className === 'open') {
+    menu.className = 'closed';
+  } else {
+    menu.className = 'open';
+  }
+});
